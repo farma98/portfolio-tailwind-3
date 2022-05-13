f27bd88deb7eda9ce37ee32a08fb4657c4388c44
Mahfud Prasetyo <37892922+farma98@users.noreply.github.com>
2022-05-13 18:33:51 +0700

    portfolio-tailwind-3

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "75px",
+    },
+    extend: {
+      colors: {
+        primary: "#22c55e",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
