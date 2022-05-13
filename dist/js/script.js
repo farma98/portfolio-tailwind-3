3ccc88ac831aeaf5c691cf0a8bab96809c9c4d1c
Mahfud Prasetyo <37892922+farma98@users.noreply.github.com>
2022-05-13 22:14:52 +0700

    update-dark-mode + perbaikan fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,17 @@
 // Navbar Fixed
 window.onscroll = function () {
   const header = document.querySelector("header");
+  const toTop = document.querySelector("#totop");
   const fixedNav = header.offsetTop;
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -18,3 +23,36 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Click On Out Box Hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Dark Mode
+const darkToogle = document.querySelector("#darkToogle");
+const html = document.querySelector("html");
+
+darkToogle.addEventListener("click", function () {
+  if (darkToogle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// Change position toogle mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToogle.checked = true;
+} else {
+  darkToogle.checked = false;
+}
